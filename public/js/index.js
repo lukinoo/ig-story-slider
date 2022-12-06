8e485ea8ee118dee1ef2cdb04bc351e829324fee
lukinoo <luka.bliadze.2015@mail.ru>
2022-12-06 23:46:44 +0400

    adding javascript code

---
+++ public/js/index.js
@@ -0,0 +1,43 @@
+const container = document.querySelector(".container");
+const add_story_button = document.getElementById("add-story-btn");
+
+const API_URI = "https://randomuser.me/api/";
+const QUANTITY = 16;
+
+const createUserStory = (data) => {
+  const story = document.createElement("div");
+  story.classList.add("container--wrapper");
+
+  const {
+    name: { first },
+    picture: { large },
+  } = data;
+
+  story.innerHTML = `
+        <div class="container--img_wrapper">
+          <img
+            src="${large}"
+            alt="user"
+            class="container--img"
+          />
+        </div>
+        <h3 class="container--user__title">${first}</h3>
+  `;
+
+  container.appendChild(story);
+};
+
+const fetchRandomUser = async () => {
+  try {
+    const req = await fetch(API_URI);
+    const json = await req.json();
+
+    createUserStory(json.results[0]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+add_story_button.addEventListener("click", () => {
+  fetchRandomUser();
+});
